678cfe337432a9b392f5cfb5a51ba86a651a8848
nimishag2000 <nimishag2000@yahoo.com>
2013-09-18 10:19:26 +0530

    test comment 2
    test comment 2

--- src/com/phonegap/helloworld/App.java
+++ src/com/phonegap/helloworld/App.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import com.phonegap.*;
 import android.os.Bundle;
 /* Test comment 1 */
-
+/* Test comment 2 */
 public class App extends DroidGap {
     /** Called when the activity is first created. */
     @Override
